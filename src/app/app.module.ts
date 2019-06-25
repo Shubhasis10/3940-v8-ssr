92cb69990b9a5b15ad026ae4be5cda95f2cba166
Matt Hippely <mhippely@vmware.com>
2019-06-24 17:37:06 -0700

    Changes to build and run app w/ ssr.
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  ClarityModule,
+  ClrEmphasisModule,
+  ClrDataModule,
+  ClrIconModule,
+  ClrModalModule,
+  ClrLoadingModule,
+  // ClrConditionalModule,
+  // ClrFocusTrapModule,
+  ClrButtonModule,
+  ClrFormsModule,
+  ClrLayoutModule,
+  ClrPopoverModule,
+  ClrWizardModule,
+  ClrDragAndDropModule,
+  // ClrStepperModule
+} from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    ClarityModule,
+    // ClrEmphasisModule,
+    // ClrDataModule,
+    // ClrIconModule,
+    // ClrModalModule,
+    // ClrLoadingModule,
+    // ClrConditionalModule,
+    // ClrFocusTrapModule,
+    // ClrButtonModule,
+    // ClrFormsModule,
+    // ClrLayoutModule,
+    // ClrPopoverModule,
+    // ClrWizardModule,
+    // ClrDragAndDropModule,
+    // ClrStepperModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
